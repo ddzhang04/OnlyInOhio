300eab0db0c95e1967fd5befff2ef26450c669f9
Anthony Zhao <anthonyzhao23@gmail.com>
2024-12-01 12:37:43 -0800

    Implemented Gojo Game backshots

--- Assets/Scripts/SaveMe.cs
+++ Assets/Scripts/SaveMe.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SaveMe : MonoBehaviour
 {
-    private static GameObject[] persistentObjects = new GameObject[5];
+    private static GameObject[] persistentObjects = new GameObject[8];
     public int objectIndex; 
 
     // Start is called before the first frame update
